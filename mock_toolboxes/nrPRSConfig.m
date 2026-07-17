4edcf8ef16d13007e2b6b74df2597c9ad4ab0929
Learner <48488291+m0bien@users.noreply.github.com>
2026-07-17 21:31:25 +0300

    Add/Update mock_toolboxes/nrPRSConfig.m

---
+++ mock_toolboxes/nrPRSConfig.m
@@ -0,0 +1,15 @@
+classdef nrPRSConfig < handle
+    properties
+        PRSResourceSetPeriod = [10 0]
+        PRSResourceOffset = 0
+        PRSResourceRepetition = 1
+        PRSResourceTimeGap = 1
+        NumRB = 52
+        RBOffset = 0
+        CombSize = 4
+        REOffset = 0
+        NPRSID = 0
+        NumPRSSymbols = 12
+        SymbolStart = 0
+    end
+end
