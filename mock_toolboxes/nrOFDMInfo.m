ad003dea66629147190f9de53f0962805b4427bf
Learner <48488291+m0bien@users.noreply.github.com>
2026-07-18 12:10:33 +0300

    Add/Update mock_toolboxes/nrOFDMInfo.m

--- mock_toolboxes/nrOFDMInfo.m
+++ mock_toolboxes/nrOFDMInfo.m
@@ -6,21 +6,28 @@ function info = nrOFDMInfo(carrier)
     % Find smallest standard FFT size
     Nfft = 2^nextpow2(nSubcarriers);
     if Nfft < 128
-        Nfft = 128; % minimum standard FFT size
+        Nfft = 128;
     end
     
     SampleRate = Nfft * scs * 1000;
     
-    % CP lengths for Normal CP
+    % CP lengths for one slot (14 symbols)
     cp0_7 = round(Nfft * 160 / 2048);
     cp_other = round(Nfft * 144 / 2048);
     
-    CyclicPrefixLengths = repmat(cp_other, 1, 14);
-    CyclicPrefixLengths(1) = cp0_7;
-    CyclicPrefixLengths(8) = cp0_7;
+    cpSlot = repmat(cp_other, 1, 14);
+    cpSlot(1) = cp0_7;
+    cpSlot(8) = cp0_7;
+    
+    % nrOFDMInfo returns CP and Symbol lengths for a SUBFRAME.
+    % Number of slots in a subframe:
+    slotsPerSubframe = scs / 15;
+    
+    % Repeat slot CP lengths for all slots in the subframe
+    CyclicPrefixLengths = repmat(cpSlot, 1, slotsPerSubframe);
     
     info.Nfft = Nfft;
     info.CyclicPrefixLengths = CyclicPrefixLengths;
     info.SampleRate = SampleRate;
-    info.SymbolLengths = repmat(Nfft, 1, 14) + CyclicPrefixLengths;
+    info.SymbolLengths = repmat(Nfft, 1, 14 * slotsPerSubframe) + CyclicPrefixLengths;
 end
